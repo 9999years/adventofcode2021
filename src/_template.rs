819873f70e35e9f1ee6e73ea6573147fff27dcc8
Rebecca Turner <rbt@sent.as>
2021-12-11 13:00:16 -0500

    Add template

---
+++ src/_template.rs
@@ -0,0 +1,44 @@
+use crate::*;
+
+type Input = ();
+
+pub fn part_1(input: Input) -> usize {
+    0
+}
+
+pub fn part_2(input: Input) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn sample() -> Input {
+        todo!()
+    }
+
+    fn input() -> Input {
+        todo!()
+    }
+
+    #[test]
+    fn test_part_1_sample() {
+        assert_eq!(part_1(sample()), 0);
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(input()), 0);
+    }
+
+    #[test]
+    fn test_part_2_sample() {
+        assert_eq!(part_2(sample()), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(input()), 0);
+    }
+}
